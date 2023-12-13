6418eb98eb586cd68b59fce92ab0d6a8f37a6bd4
Nico Rehwaldt <git_nikku@nixis.de>
2023-12-13 22:26:18 +0100

    chore: update test depencencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,12 @@
 /* eslint-env node */
 
 // configures browsers to run test against
-// any of [ 'Chrome', 'Firefox' ]
-var browsers =
-  (process.env.TEST_BROWSERS || 'ChromeHeadless')
-    .replace(/^\s+|\s+$/, '')
-    .split(/\s*,\s*/g);
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox' ]
+const browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
+
+// use puppeteer provided Chrome for testing
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 
 module.exports = function(karma) {
 
@@ -26,9 +27,7 @@ module.exports = function(karma) {
 
     reporters: [ 'progress' ],
 
-    browsers: browsers,
-
-    browserNoActivityTimeout: 30000,
+    browsers,
 
     singleRun: true,
     autoWatch: false,
